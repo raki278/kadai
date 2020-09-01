ac829b33ec217faf105d57b64072f028ef13bd70
fuka-show <68270717+fuka-show@users.noreply.github.com>
2020-09-01 19:45:48 +0900

    A課題でボタンを押せるプログラムです

---
+++ kadai1/kadai1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Kadai:Form
+{
+    private Label lb;
+    private Button bt;
+
+
+    public static void Main()
+    {
+        Application.Run(new Kadai());
+    }
+    public Kadai()
+    {
+        this.Text = "課題です。";
+        this.Width = 450; this.Height= 300;
+
+        lb = new Label();
+        lb.Text = "友達と一緒に参考書";
+        lb.Width = 150;
+        bt = new Button();
+        bt.Text = "友達と一緒にやりました。";
+        bt.Top = this.Top + lb.Height;
+        bt.Width = lb.Width;
+
+        bt.Parent = this;
+        lb.Parent = this;
+
+        bt.Click += new EventHandler(bt_Click);
+    }
+    public void bt_Click(object sender, EventArgs e)
+    {
+        lb.Text = "見ながらやりました。";
+    }
+}
